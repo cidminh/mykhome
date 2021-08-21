7a05f48ac0d2d705359d43461f77cd1b7ad809f1
cidminh <cidminh@gmail.com>
2021-08-21 19:25:46 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Hee luu bé!',
+    introDesc: `Anh có điều này muốn hỏi bé, nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'oke',
+    title: 'Bé có yêu anh không 😙',
+    desc: 'Bé không trả lời hoặc thoát ra, tức là em là vợ anh đó nha :3 ',
+    btnYes: 'yêu ơi là iuuu <33',
+    btnNo: 'Anh điên à :333',
+    question:'Nói cho anh lý do vì sao em yêu anh đii <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Tại vì anh đẹp trai khoai to hihi <33333333',
+    mess: 'Anh biết mà 🥰. Love you 3000 😘😘',
+    messDesc: 'còn giờ thì chờ gì nữa mà không vào zalo xem hình anh nào :3.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://zalo.me/0936347287' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
